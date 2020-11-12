1e7f598aeed194e2d27185e58fd5f03200c5f40c
Dan Lebrero <dlebrero@gmail.com>
2020-11-12 09:58:20 +0100

    Update and rename monorepo-build.sh to monorepo-build-semaphore.sh

--- monorepo-build-semaphore.sh
+++ monorepo-build-semaphore.sh
@@ -27,10 +27,10 @@ echo "commits content: ${COMMIT_CONTENT}"
 DIRS=$(echo "${COMMIT_CONTENT}" | grep ".*/.*/.*" | cut -f -2 -d/ | sort -u)
 
 while read -r line; do
-    if [[ -f "${line}/ci/build.sh" ]]; then
+    if [[ -f "${line}/ci/build-semaphore.sh" ]]; then
         echo "Building ${line}"
         pushd "${line}"
-        if [[ -f "ci/deploy.sh" ]]; then
+        if [[ -f "ci/deploy-semaphore.sh" ]]; then
             echo "Deploying ${line}"
         fi
         popd
